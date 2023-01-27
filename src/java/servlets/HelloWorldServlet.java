2c2ae51b745cde8657e9e7811c3770121833e1a6
KELSEY-LAPTOP\Kelsey <k.whitlock@hotmail.ca>
2023-01-27 13:42:04 -0700

    Converted application into a full MVC  pattern

--- src/java/servlets/HelloWorldServlet.java
+++ src/java/servlets/HelloWorldServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Person;
 
 /**
  *
@@ -26,12 +27,11 @@ public class HelloWorldServlet extends HttpServlet {
         String firstname = request.getParameter("firstname");
         String lastname = request.getParameter("lastname");
         
-        // set the attributes for the JSP
-        request.setAttribute("firstname", firstname);
-        request.setAttribute("lastname", lastname);
-        
         // validation: if the parameters don't exist or are empty, show the first page again
         if (firstname == null || firstname.equals("") || lastname == null || lastname.equals("")) {
+             request.setAttribute("firstname", firstname);
+              request.setAttribute("lastname", lastname);
+        
             // set an attribute for a message
             request.setAttribute("message", "Invalid entry. Please enter both your first and last names.");
             
@@ -41,6 +41,10 @@ public class HelloWorldServlet extends HttpServlet {
                     .forward(request, response);
             return; // very important!
         }
+        
+        Person person = new Person(firstname, lastname);
+        request.setAttribute("person", person);
+        
 
         // display the second page
         getServletContext().getRequestDispatcher("/WEB-INF/sayHello.jsp")
